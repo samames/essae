eb4c358814bc13777e183b98625aa48bc71cf855
sam ames <samames@gmail.com>
2020-11-12 22:36:55 +0000

    prep for dep2

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,19 @@ app.use(express.json());
 
 app.use(emailRouter);
 
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('../frontend/build'));
+const __dirname = path.resolve();
 
-  app.get('*', (req, res) => {
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
     res.sendFile(
       path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
-    );
+    )
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
   });
 }
 
