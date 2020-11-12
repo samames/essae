2ef84a676186ddaf7e1e306e751530fbf6657423
sam ames <samames@gmail.com>
2020-11-12 23:54:06 +0000

    edgf

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,12 @@ app.use(express.json());
 app.use(emailRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(__dirname + '/frontend/build'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(
+      path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')
+    )
   );
 } else {
   app.get('/', (req, res) => {
